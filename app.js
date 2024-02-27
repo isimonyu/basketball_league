67a3317a1bd84b69ac97b4432a2f66bd5e0fbd8e
Simon Yu <isimonyu@gmail.com>
2024-02-27 18:29:02 -0500

    updated a lot

--- app.js
+++ app.js
@@ -24,13 +24,60 @@ app.get("/", function (req, res) {
   res.render("index"); // Note the call to render() and not send(). Using render() ensures the templating engine
 }); // will process this file, before sending the finished HTML to the client.
 
-app.get("/teams", function (req, res) {
-  let query1 = "SELECT * FROM Teams;";
-
+app.get("/players", function (req, res) {
+  let query1 =
+    "SELECT playerID as ID, playerName as Name, playerHeight as Height, playerPosition as Position, playerNumber as Number, Teams.teamName as Team FROM Players LEFT JOIN Teams ON Players.teamID = Teams.teamID;";
   db.pool.query(query1, function (error, rows, fields) {
-    // Execute the query
+    res.render("players/players", { data: rows });
+  });
+});
 
-    res.render("teams", { data: rows }); // Render the index.hbs file, and also send the renderer
+app.get("/coaches", function (req, res) {
+  let query1 =
+    "SELECT coachID as ID, teamName as Team, coachName as Name, coachStyle as Style, yearsEXP as Experience, totalWin as Wins, totalLoss as Losses FROM Coaches JOIN Teams ON Coaches.teamID = Teams.teamID;";
+  db.pool.query(query1, function (error, rows, fields) {
+    res.render("coaches", { data: rows });
+  });
+});
+
+app.get("/teams", function (req, res) {
+  let query1 = "SELECT teamName as Name, city as City FROM Teams;";
+  db.pool.query(query1, function (error, rows, fields) {
+    res.render("teams/teams", { data: rows });
+  });
+});
+
+app.get("/games", function (req, res) {
+  let query1 = `SELECT Games.gameID as ID, Games.date as Date, H.teamName as 'Home Team', Games.homeTeamScore as 'Home Score', A.teamName as 'Away Team', Games.awayTeamScore as 'Away Score'
+  FROM Games
+  JOIN TeamsGames Home ON Games.gameID  = Home.gameID AND Home.isHome = True
+  JOIN Teams H ON Home.teamID = H.teamID
+  JOIN TeamsGames Away ON Games.gameID  = Away.gameID AND Away.isHome = False
+  JOIN Teams A ON Away.teamID = A.teamID;`;
+  db.pool.query(query1, function (error, rows, fields) {
+    res.render("games", { data: rows });
+  });
+});
+
+app.get("/stats", function (req, res) {
+  let query1 = `SELECT Games.date as Date, Players.playerName as Name,
+  point as Points, assist as Assists, rebound as Rebounds, CONCAT(fgMake, '/', fgAttempt) as 'FGM/FGA', CONCAT(ftMake, '/', ftAttempt) as 'FTM/FTA', CONCAT(threePointMake, '/', threePointAttempt) as '3PM/3PA', block as Blocks, steal as Steals, playerFoul as Fouls, playerMinute as Minutes 
+  FROM PlayersGamesStats 
+  JOIN Players ON PlayersGamesStats.playerID = Players.playerID
+  JOIN Games ON PlayersGamesStats.gameID = Games.gameID;`;
+  db.pool.query(query1, function (error, rows, fields) {
+    res.render("stats", { data: rows });
+  });
+});
+
+app.get("/teams/create", function (req, res) {
+  let query1 = `CREATE TABLE Teams (
+    teamID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    teamName varchar(70) UNIQUE NOT NULL,
+    city varchar(70) NOT NULL,
+    PRIMARY KEY (teamID));`;
+  db.pool.query(query1, function (error, rows, fields) {
+    res.render("teams/addteam");
   });
 });
 
