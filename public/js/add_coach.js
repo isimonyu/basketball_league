c11bd2203c43217f0fa7b4be907f224cd1e56eef
Simon Yu <isimonyu@gmail.com>
2024-02-29 17:42:46 -0500

    added functionality for Coaches

---
+++ public/js/add_coach.js
@@ -0,0 +1,47 @@
+let addCoachForm = document.getElementById("coach-create-form");
+
+addCoachForm.addEventListener("submit", async function (e) {
+  // Prevent the form from submitting
+  e.preventDefault();
+
+  let inputCoachName = document.getElementById("input-cname");
+  let inputStyle = document.getElementById("input-style");
+  let inputExperience = document.getElementById("input-experience");
+  let inputWin = document.getElementById("input-win");
+  let inputLoss = document.getElementById("input-loss");
+  let inputTeamID = document.getElementById("input-teamID");
+
+  let coachNameValue = inputCoachName.value;
+  let styleValue = inputStyle.value;
+  let experienceValue = inputExperience.value;
+  let winValue = inputWin.value;
+  let lossValue = inputLoss.value;
+  let teamIDValue = inputTeamID.value;
+
+  let data = {
+    coachName: coachNameValue,
+    style: styleValue,
+    experience: experienceValue,
+    win: winValue,
+    loss: lossValue,
+    teamID: teamIDValue,
+  };
+
+  fetch("/coaches/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (res.ok) {
+        window.location.href = "/coaches";
+      } else {
+        console.log("Error adding coach");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
