ed4d587ea8abe0c770c4819b5f8d0e5c5ab3d36f
Simon Yu <isimonyu@gmail.com>
2024-03-08 00:01:57 -0500

    reverted consolidation

---
+++ public/js/edit_coach.js
@@ -0,0 +1,82 @@
+let editCoachForm = document.getElementById("coach-edit-form");
+
+function validateForm() {
+  let inputCoachName = document.getElementById("input-cname").value;
+  let inputStyle = document.getElementById("input-style").value;
+  let inputExperience = document.getElementById("input-experience").value;
+  let inputWin = document.getElementById("input-win").value;
+  let inputLoss = document.getElementById("input-loss").value;
+  let inputTeamID = document.getElementById("input-teamID").value;
+
+  if (inputCoachName.length <= 0) {
+    alert(`Coach name cannot be empty`);
+    return false;
+  }
+  if (inputStyle.length <= 0) {
+    alert(`Coaching style cannot be empty`);
+    return false;
+  }
+  if (inputExperience < 0) {
+    alert(`Coaching experience cannot be negative`);
+    return false;
+  }
+  if (inputWin < 0) {
+    alert(`Coaching wins cannot be negative`);
+    return false;
+  }
+  if (inputLoss < 0) {
+    alert(`Coaching losses cannot be negative`);
+    return false;
+  }
+  if (inputTeamID == "") {
+    alert(`Team cannot be empty`);
+    return false;
+  }
+  return true;
+}
+
+editCoachForm.addEventListener("submit", async function (e) {
+  // Prevent the form from submitting
+  e.preventDefault();
+
+  let id = document.getElementById("input-id".value);
+  let inputCoachName = document.getElementById("input-cname").value;
+  let inputStyle = document.getElementById("input-style").value;
+  let inputExperience = document.getElementById("input-experience").value;
+  let inputWin = document.getElementById("input-win").value;
+  let inputLoss = document.getElementById("input-loss").value;
+  let inputTeamID = document.getElementById("input-teamID").value;
+
+  if (!validateForm()) {
+    return false;
+  }
+
+  let data = {
+    coachName: inputCoachName,
+    style: inputStyle,
+    experience: inputExperience,
+    win: inputWin,
+    loss: inputLoss,
+    teamID: inputTeamID,
+  };
+
+  console.log(data);
+
+  fetch(`/coaches/edit/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (res.ok) {
+        window.location.href = "/coaches";
+      } else {
+        console.log("Error editing coach");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
--- public/js/edit_game.js
+++ public/js/edit_game.js
@@ -1,3 +1,5 @@
+let editGameForm = document.getElementById("game-edit-form");
+
 function validateForm() {
   let inputDate = document.getElementById("input-date").value;
   let inputHomeTeamID = document.getElementById("input-hteamID").value;
@@ -30,54 +32,6 @@ function validateForm() {
     return false;
   }
 }
-
-// ADD Game
-let addGameForm = document.getElementById("game-create-form");
-
-addGameForm.addEventListener("submit", async function (e) {
-  // Prevent the form from submitting
-  e.preventDefault();
-
-  let inputDate = document.getElementById("input-date").value;
-  let inputHomeTeamID = document.getElementById("input-hteamID").value;
-  let inputAwayTeamID = document.getElementById("input-ateamID").value;
-  let inputHomeScore = document.getElementById("input-hscore").value;
-  let inputAwayScore = document.getElementById("input-ascore").value;
-
-  if (!validateForm()) {
-    return false;
-  }
-
-  let data = {
-    date: inputDate,
-    homeTeamID: inputHomeTeamID,
-    awayTeamID: inputAwayTeamID,
-    homeScore: inputHomeScore,
-    awayScore: inputAwayScore,
-  };
-
-  fetch("/games/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => {
-      if (res.ok) {
-        window.location.href = "/games";
-      } else {
-        console.log("Error adding game");
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
-
-// EDIT Game
-
-let editGameForm = document.getElementById("game-edit-form");
 
 editGameForm.addEventListener("submit", async function (e) {
   // Prevent the form from submitting
@@ -120,38 +74,3 @@ editGameForm.addEventListener("submit", async function (e) {
       console.error("Error:", error);
     });
 });
-
-// DELETE Game
-async function deleteGame(gameID, gameDate) {
-  let userConfirmation = window.confirm(
-    `Are you sure you want to delete game on ${gameDate}?`
-  );
-  if (userConfirmation) {
-    // OK to delete
-    let data = {
-      gameID: gameID,
-    };
-
-    fetch("/games/delete", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => {
-        if (res.ok) {
-          window.location.href = "/games";
-        } else {
-          console.log("Error deleting game");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
-}
-
-async function editGame(gameID) {
-  window.location.href = `/games/edit/${gameID}`;
-}
